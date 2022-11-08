521150909a136bc4ceaa14ae302091c3a0a7c3d6
spatil62 <112513163+spatil62@users.noreply.github.com>
2022-11-08 17:29:40 -0600

    changes check

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+ //window.addEventListener("click",myfunction);
+ function myfunction()
+ {
+
+   var input = document.getElementById('entryinput');
+   alert("Sneha Sunil Patil :" + input.value);
 
 
+ }
 
-
-
-
-
+   var clickbutton = document. getElementById('entrybutton');
+   clickbutton.addEventListener('click' , myfunction);
+}
 window.addEventListener('load', init);
