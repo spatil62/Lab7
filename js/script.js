069387150d9a537a7504f9e9468e082e29d012a9
spatil62 <112513163+spatil62@users.noreply.github.com>
2022-11-08 19:06:01 -0600

    basics and indentation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
-function init(){
-//add your javascrip between these two lines of code
- //window.addEventListener("click",myfunction);
- function myTaskFunction()
- {
+function init() {
+  //add your javascrip between these two lines of code
+  //window.addEventListener("click",myfunction);
+  function myTaskFunction() {
 
-   var inputval = document.getElementById('entryinput');
+    var inputval = document.getElementById('entryinput');
+    var outputval = document.getElementById('textoutput');
 
-   var outputval = document.getElementById('textoutput');
-   alert("Sneha Sunil Patil :" + inputval.value);
-
-   outputval.innerHTML = inputval;
- }
-
-   var clickbutton = document. getElementById('entrybutton');
-   clickbutton.addEventListener('click', myTaskFunction);
+    alert("Sneha Patil :" + inputval.value);
+    outputval.innerHTML = inputval.value;
+    
+  }
+  var clickbutton = document.getElementById('entrybutton');
+  clickbutton.addEventListener('click', myTaskFunction);
 }
 window.addEventListener('load', init);
